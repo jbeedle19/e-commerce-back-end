e2538ad465d3294aa988dc183d4fb1d1a31a4576
jbeedle19 <josh.beedle@gmail.com>
2021-01-29 16:50:39 -0500

    updated associations to include foreignKey

--- models/index.js
+++ models/index.js
@@ -6,19 +6,26 @@ const ProductTag = require('./ProductTag');
 
 // Associations
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: 'product_id'
+});
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
+// Do I need ( as: 'product_tag' ) for these associations??
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag
+  through: ProductTag,
+  foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag
+  through: ProductTag,
+  foreignKey: '_id'
 });
 
 module.exports = {
